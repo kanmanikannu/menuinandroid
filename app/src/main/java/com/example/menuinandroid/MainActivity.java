cfedac3ce11c239bcb49f91ab3045b76501c11b2
kanmanikannu <114866367+kanmanikannu@users.noreply.github.com>
2024-05-06 08:35:39 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/menuinandroid/MainActivity.java
+++ app/src/main/java/com/example/menuinandroid/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.menuinandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the toolbar as the action bar
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option, menu);
+        return true;
     }
 }
